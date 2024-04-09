d0aff30e1b5680aee141ad659c7f8e895455426b
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-09 12:21:33 +0300

    added requests

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded",()=>{
+const likeButton = document.getElementById("likebutton")
+const themeChanger = document.getElementById("themechanger")
+const  newQuote = document.getElementById("new quote")
+  
+newQuote.addEventListener("click",()=>{
+fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+.then(res=>res.json)    
+})
 
-    
+
+
 })
