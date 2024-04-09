fb7e76133384d58d5d31568a825ad193108632c3
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-09 14:37:31 +0300

    updated theme event listener

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ document.addEventListener("DOMContentLoaded",()=>{
 const likeButton = document.getElementById("likebutton")
 const themeChanger = document.getElementById("themechanger")
 const  newQuote = document.getElementById("new quote")
+const body = document.querySelector("body") 
   
 newQuote.addEventListener("click",()=>{
 fetch("https://api.breakingbadquotes.xyz/v1/quotes")
-.then(res=>res.json)    
+.then(res=>res.json())
+.then(quoteData =>{
+const quoteText = quoteData[0].quote
+const authorText = quoteData[0].author
+ const quotes = document.createElement("p")
+  body.appendChild(quotes)
+  quotes.textContent = `${quoteText} - ${authorText} ` 
+} )
+.catch( error => {
+    console.log ( `Error fetching quote , ${error}`)
+})    
 })
+themeChanger.addEventListener ("click",switchTheme)
 
-
+function switchTheme(){
+    
+}
 
 })
