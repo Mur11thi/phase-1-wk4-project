820204eb3d4f11a939a43d1ffcc9184adec8d9d5
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-10 10:17:43 +0300

    updates like button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",()=>{
-const likeButton = document.getElementById("likebutton")
+
 const themeChanger = document.getElementById("themechanger")
 const  newQuote = document.getElementById("newquote")
 const body = document.querySelector("body") 
@@ -8,15 +8,18 @@ newQuote.addEventListener("click",()=>{
 fetch("https://api.breakingbadquotes.xyz/v1/quotes")
 .then(res=>res.json())
 .then(quoteData =>{
+ quoteData.forEach(quote=>{   
 const quoteText = quoteData[0].quote
 const authorText = quoteData[0].author
  const quotes = document.createElement("p")
   body.appendChild(quotes)
-  quotes.textContent = `${quoteText} - ${authorText} ` 
-//   forEach(quoteData{
-    
-//   })
-} )
+  quotes.textContent = `${quoteText} - ${authorText} `
+ const  likeButton = document.createElement("button")
+ likeButton.textContent = "Like"
+ 
+ body.appendChild(likeButton)  
+
+} ) })
 .catch( error => {
     console.log ( `Error fetching quote , ${error}`)
 })    
