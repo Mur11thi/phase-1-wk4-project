d9d4a3f8d803d2ed25e9c20b40526197bb5ab4f4
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-11 23:11:12 +0300

    final comments update

--- index.js
+++ index.js
@@ -27,31 +27,34 @@ const authorText = quoteData[0].author //=>Assigns the actual author specificall
  likeButton.id = "likebutton"    //=>Sets the likeButton'S id to "likebutton" 
  likeButton.classList.add("likebutton")//=>Adds the likeButton to the classList of "likeButton" where it can 
                                        //receive styling from the external css file.  
- body.appendChild(likeButton)          //=>Appends the likeButton to the body just below the quotes generated. 
-
- const form = document.createElement("form")
- const firstInput =document.createElement("input")
-  firstInput.id = "comments"
-  firstInput.type = "text"
-  firstInput.placeholder = "Add a comment"
-  const secondInput=document.createElement("input")
-  secondInput.id = "send"
-  secondInput.type ="submit" 
-    form.append(firstInput)
-    form.append(secondInput)
-    body.insertBefore(form,likeButton.nextSibling) 
+ body.appendChild(likeButton)          //=>Appends the likeButton to the body just below the quotes generated.
+ const form = document.createElement("form")//=>Variable that creates a form element
+ const firstInput =document.createElement("input")//Variable that creates input element inside the newly created 
+                                                  // form element.
+  firstInput.id = "comments"//=>Sets the firstInput id to "comments".
+  firstInput.type = "text"  //=>Sets the firstInput type to "text" .
+  firstInput.placeholder = "Add a comment" //=>Sets the firstInput placeholder to "Add a comment".
+  const secondInput=document.createElement("input")//=>secondInput variable that creates the input element for the
+                                            //for the submit button that comes along with the comments search bar  
+  secondInput.id = "send"//=> Assigns the secondInput's Id to "send"                         
+  secondInput.type ="submit"//=>Sets the secondInput's type to "submit" . 
+    form.append(firstInput) //=>Appends firstInput to the form element
+    form.append(secondInput) //Appends secondInput to the form element
+    body.insertBefore(form,likeButton.nextSibling)//=>Inserts the form element before the likeButtons' next sibling
+                                                  // and just after the likeButton. 
     
-    form.addEventListener("submit",(e)=>{
+    form.addEventListener("submit",(e)=>{ /* is an event listener that listens to the submit event , and prevents reloading of the  webpage by preventing its default behaviour .A variable  is assigned to the value typed in to the comment box.For every submit event, a "p"  element is created where the comment will be stored in.*/
         e.preventDefault()
         const commentMessage = firstInput.value
       const commentDisplay =document.createElement("p")
-      commentDisplay.textContent = commentMessages
+      commentDisplay.textContent = commentMessage
       body.append(commentMessage)
       
 
     }) 
      
-  likeButton.addEventListener("click",()=>{
+  likeButton.addEventListener("click",()=>{ /* Is an event listener that listens for the click event and then performs a function that sets the likeButton's textcontent to "Liked!" and the likeButton's color to red.
+  A variable that creates a "p" element where the likeMessage will be stored.The likeMessage's  textContent is set to "You liked this quote!" and its style and color is white.The likeMessage is then appended before the likeButtons' next sibling and just after the likeButton. */
   likeButton.textContent = "Liked!" 
   likeButton.style.color = "red" 
   const likeMessage = document.createElement("p")
@@ -62,8 +65,9 @@ const authorText = quoteData[0].author //=>Assigns the actual author specificall
  })
 
 } ) })
-.catch( error => {
-    console.log ( `Error fetching quote , ${error}`)
+.catch( error => {                                   /* catches the error if the promise is rejected and sends
+                                                       a message to the console.*/
+    console.log ( `Error fetching quote , ${error}`)   
 })    
 })
 })
