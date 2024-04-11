bde6f74c101d14b2f414229282973ef950f5c622
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-11 12:31:20 +0300

    updates comments and submit button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 const themeChanger = document.getElementById("themechanger")
 const  newQuote = document.getElementById("newquote")
 const body = document.querySelector("body") 
+
   
 newQuote.addEventListener("click",()=>{
 fetch("https://api.breakingbadquotes.xyz/v1/quotes")
@@ -16,8 +17,29 @@ const authorText = quoteData[0].author
   quotes.textContent = `${quoteText} - ${authorText} `
  const  likeButton = document.createElement("button")
  likeButton.textContent = "Like"
- 
- body.appendChild(likeButton)  
+ likeButton.id = "likebutton"
+ likeButton.classList.add("likebutton")
+ body.appendChild(likeButton)
+ const form = document.createElement("form")
+ const firstInput =document.createElement("input")
+  firstInput.id = "comments"
+  firstInput.type = "text"
+  firstInput.placeholder = "Add a comment"
+  const secondInput=document.createElement("input")
+  secondInput.id = "send"
+  secondInput.type ="submit" 
+    form.append(firstInput)
+    form.append(secondInput)
+    body.insertBefore(form,likeButton.nextSibling)  
+  likeButton.addEventListener("click",()=>{
+  likeButton.textContent = "Liked!" 
+  likeButton.style.color = "red" 
+  const likeMessage = document.createElement("p")
+  likeMessage.textContent= "You liked this quote!"
+  likeMessage.style.color = "white"
+  body.insertBefore(likeMessage, likeButton.nextSibling)
+  
+ })
 
 } ) })
 .catch( error => {
